6b3389fa1820621a0372097fef81518a90bd5028
davidchkh5 <daviti.chkhaidze5@gmail.com>
2024-11-13 23:51:03 +0400

    Added searching and searchreset things

--- app/auctions/Filters.tsx
+++ app/auctions/Filters.tsx
@@ -1,15 +1,15 @@
+import { useParamsStore } from '@/hooks/useParamsStore';
 import { Button, ButtonGroup } from 'flowbite-react';
 import React from 'react'
 
 
-type Props = {
-    pageSize: number;
-    setPageSize: (size: number) => void;
-}
-
 const pageSizeButtons = [4, 8, 12];
 
-export default function Filters({pageSize, setPageSize}:Props) {
+export default function Filters() {
+
+    const pageSize = useParamsStore(state => state.pageSize);
+    const setParams = useParamsStore(state => state.setParams);
+    
   return (
     <div className='flex justify-between items-center mb-4'>
         <span className='uppercase text-sm text-gray-500 mb-2'>Page Size</span>
@@ -17,7 +17,7 @@ export default function Filters({pageSize, setPageSize}:Props) {
         <ButtonGroup>
             {pageSizeButtons.map((value, i) => (
 
-                <Button key={i} onClick={() => setPageSize(value)}
+                <Button key={i} onClick={() => setParams({pageSize: value})}
                 color={`${pageSize === value ? 'red' : 'gray'}`}
                 >
                     {value}
